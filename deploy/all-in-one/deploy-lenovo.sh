fcfbe477ebe93289551abd49d5dceffc8d4f66b3
vietlubu <baoviet3010@gmail.com>
2026-09-14 11:31:32 +0700

    update script deploy

--- deploy/all-in-one/deploy-lenovo.sh
+++ deploy/all-in-one/deploy-lenovo.sh
@@ -2,7 +2,7 @@
 set -eu
 
 branch=${1:-test-server}
-expected_data_version=126
+expected_data_version=128
 repo_root=$(CDPATH= cd -- "$(dirname -- "$0")/../.." && pwd)
 lan_config_dir=/home/vietlubu/.config/openmu
 compose_dir="$repo_root/deploy/all-in-one"
